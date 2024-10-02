a6f56aaa3dae1d98e89d0a02fdc61cf41df5d7cd
Narendar761 <145740954+Narendar761@users.noreply.github.com>
2024-10-02 16:19:10 +0530

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,11 +1,11 @@
 // ---------- Insert Your Data ---------- //
 
-const BOT_TOKEN = "BOT_TOKEN"; // Insert your bot token.
+const BOT_TOKEN = "7618077253:AAFgMttEwWGhdxqqjYxBLJDWCNa_IYkgylQ"; // Insert your bot token.
 const BOT_WEBHOOK = "/endpoint"; // Let it be as it is.
 const BOT_SECRET = "BOT_SECRET"; // Insert a powerful secret text.
-const BOT_OWNER = 123456789; // Insert your telegram account id.
-const BOT_CHANNEL = -100123456789; // Insert your telegram channel id which the bot is admin in.
-const SIA_NUMBER = 1234; // Insert a random integer number and keep it safe.
+const BOT_OWNER = 819861991; // Insert your telegram account id.
+const BOT_CHANNEL = -1001916142483; // Insert your telegram channel id which the bot is admin in.
+const SIA_NUMBER = 14327; // Insert a random integer number and keep it safe.
 
 // ---------- Do Not Modify ---------- // 
 
